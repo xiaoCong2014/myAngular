f66f317eaedd5f2159eb2e3b68856413f8b16704
1149023208@qq.com <zxxxxx11>
2017-12-17 19:26:12 +0800

    换行

--- aio/content/examples/toh-pt6/src/app/dashboard/dashboard.component.ts
+++ aio/content/examples/toh-pt6/src/app/dashboard/dashboard.component.ts
@@ -14,11 +14,16 @@ export class DashboardComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    // console.info("DashboardComponent ngOnInit");
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+    this.heroService
+      .getHeroes()
+      .subscribe(
+        heroes =>
+          this.heroes = heroes.slice(1, 5)
+      );
   }
 }

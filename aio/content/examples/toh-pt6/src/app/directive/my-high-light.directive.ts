96c1ece373b04ed928514298324123f895bc9834
1149023208@qq.com <zxxxxx11>
2017-12-31 21:38:09 +0800

    属性指令

---
+++ aio/content/examples/toh-pt6/src/app/directive/my-high-light.directive.ts
@@ -0,0 +1,42 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+
+@Directive({
+  selector: '[myHighLightDirective]'
+})
+export class MyHighLightDirective {
+
+  constructor(private el: ElementRef) { }
+
+  // #docregion defaultColor
+  @Input('defaultColor') defaultColor: string;
+  // #enddocregion defaultColor
+
+  // #docregion color
+  // @Input('appHighlight') highlightColor: string;
+
+  @Input('highlightColor') highlightColor: string;
+  @Input('highlightColor2') highlightColor2: string;
+
+
+  // #enddocregion color
+
+  // #docregion mouse-enter
+  @HostListener('mouseenter') onMouseEnter() {
+    console.info("highlightColor " + this.highlightColor );
+    // console.info("highlightColor2 " + this.highlightColor2 );
+
+
+    this.highlight(this.highlightColor || this.defaultColor || 'red');
+  }
+  // #enddocregion mouse-enter
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
+}
